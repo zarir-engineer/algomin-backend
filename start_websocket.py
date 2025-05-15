ee87c33bbfcd867c3404510c92be4f69b5cfa49f
Zarir Engineer <zarir.utube@gmail.com>
2025-05-15 21:47:44 +0530

    update

--- start_websocket.py
+++ start_websocket.py
@@ -22,19 +22,17 @@ start_websocket.py	                            Entry point to plug it all togeth
 '''
 
 # start_websocket.py
-from config_loader.broker_config_loader import BrokerConfigLoader
 from sessions.angelone_session import AngelOneSession
+from config_loader.broker_config_loader import BrokerConfigLoader
 from brokers.angelone_websocket_client import AngelOneWebSocketV2Client
-from strategy_loader.yaml_strategy_loader import YAMLStrategyLoader
-from brokers.angelone_trading_client import AngelOneConnectClient
 from brokers.angelone_websocket_event_handler import AngelOneWebSocketEventHandler
-from core.strategy_executor import StrategyExecutor
 
 
 def main():
     # Step 1: Load config
     config_loader = BrokerConfigLoader()
     credentials = config_loader.load_credentials()
+    # get credentials for AngelOne and what instrument needs live data
     ws_config = config_loader.load_ws_config()
 
     # Step 2: Optionally load strategies (skip for live tick test)
@@ -43,19 +41,21 @@ def main():
     # Step 3: Start session
     session = AngelOneSession(credentials)
 
-    # Step 4: Init trading client
-    trading_client = AngelOneConnectClient(session)
-    print("✅ Trading client ready")
-
-    # Step 5: Init strategy executor
-    strategy_executor = StrategyExecutor(strategies, trading_client)
-
     # Step 6: Init WebSocket client
-    ws_client = AngelOneWebSocketV2Client(session)
+    retry_config = ws_config.get("websocket.retry", {})
+    ws_client = AngelOneWebSocketV2Client(
+        session,
+        max_retry_attempt=retry_config.get("max_attempt", 3),
+        retry_strategy=retry_config.get("strategy", 1),
+        retry_delay=retry_config.get("delay", 5),
+        retry_multiplier=retry_config.get("multiplier", 2),
+        retry_duration=retry_config.get("duration", 30)
+    )
+
 
     # Step 7: Init event handler
     event_handler = AngelOneWebSocketEventHandler(
-        strategy_executor,
+        strategy_executor=None,
         correlation_id=ws_config.get("correlation_id", "sub_default"),
         mode=ws_config.get("mode", "full"),
         token_list=ws_config.get("subscriptions", []),
@@ -67,11 +67,21 @@ def main():
         event_handler.on_data,
         event_handler.on_open,
         event_handler.on_close,
-        event_handler.on_error
+        event_handler.on_error,
+        event_handler.on_control_message
     )
 
     # Step 9: Connect and wait for ticks
     ws_client.connect()
+
+    if hasattr(ws_client, "add_observer"):
+        ws_client.add_observer(event_handler)
+
+    if hasattr(ws_client, "start_heartbeat"):
+        ws_client.start_heartbeat()
+
+    ws_client.run_forever()
+    # ws_client.sws._ws.run_forever()
 
 if __name__ == "__main__":
     main()
